3a7b1868ec11911d6932be24f7e9f55659143991
jstuparitz <jstuparitz@hotmail.com>
2015-02-07 16:27:59 -0600

    new stuff

---
+++ Code/FamilyBook/FamilyBook.AzureStorageAdapter.IntegrationTests/PhotoRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using FamilyBook.AzureStorageAdapter.Photo;
+using NUnit.Framework;
+
+namespace FamilyBook.AzureStorageAdapter.IntegrationTests
+{
+    [TestFixture]
+    public class PhotoRepositoryTests : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+            //Arrange
+            byte[] picture =
+                File.ReadAllBytes(@"C:\Code\FamilyBook\FamilyBook.AzureStorageAdapter.IntegrationTests\testimage.jpg");
+            IPhotoRepository photoRepository = new PhotoRepository(BlobClient);
+            var photo = new Photo.Photo {Id = Guid.NewGuid(), PhotoImage = picture};
+
+            //Act
+            photoRepository.SavePhoto(photo);
+
+            //Assert
+        }
+    }
+}
---
+++ Code/FamilyBook/FamilyBook.AzureStorageAdapter.IntegrationTests/TestBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+
+namespace FamilyBook.AzureStorageAdapter.IntegrationTests
+{
+    [SetUpFixture]
+    public class TestBase
+    {
+        protected static CloudBlobClient BlobClient;
+        protected static CloudQueueClient QueueClient;
+        protected static CloudTableClient TableClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Create a storage account class using the credentials
+            var creds = new StorageCredentials(ConfigurationManager.AppSettings["AccountName"], ConfigurationManager.AppSettings["StorageAccountKey"]);
+            var account = new CloudStorageAccount(creds, false);
+
+            //Create a storage account using a connection string
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            CloudStorageAccount devStorageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+            BlobClient = devStorageAccount.CreateCloudBlobClient();
+            TableClient = devStorageAccount.CreateCloudTableClient();
+            QueueClient = devStorageAccount.CreateCloudQueueClient();
+
+            string storageAccountName = "familybooktest";
+            //string queueEndpointUri = "http://" + storageAccountName + ".queue.core.windows.net";
+            //var uri = new Uri(queueEndpointUri);
+            //QueueClient = new CloudQueueClient(uri, creds);
+        }
+    }
+}
